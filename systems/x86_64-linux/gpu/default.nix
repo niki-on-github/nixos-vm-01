dc6f60ebe07d3a5c55c0ebd0b39f5f7396e1bdd7
nix <nix@local>
2024-05-11 11:53:15 +0200

    remove comment

--- systems/x86_64-linux/gpu/default.nix
+++ systems/x86_64-linux/gpu/default.nix
@@ -29,21 +29,6 @@ in
       cores = 4;
     };
 
-/*
-    "-machine pc-q35-8.0,usb=off,vmport=off,dump-guest-core=off"
-      "-device '{\"driver\":\"pcie-root-port\",\"port\":16,\"chassis\":1,\"id\":\"pci.1\",\"bus\":\"pcie.0\",\"multifunction\":true,\"addr\":\"0xE\"}'"
-      "-device '{\"driver\":\"pcie-root-port\",\"port\":17,\"chassis\":2,\"id\":\"pci.2\",\"bus\":\"pcie.0\",\"addr\":\"0xE.0x1\"}'"
-      "-device '{\"driver\":\"pcie-root-port\",\"port\":18,\"chassis\":3,\"id\":\"pci.3\",\"bus\":\"pcie.0\",\"addr\":\"0xE.0x2\"}'"
-      "-device '{\"driver\":\"pcie-root-port\",\"port\":19,\"chassis\":4,\"id\":\"pci.4\",\"bus\":\"pcie.0\",\"addr\":\"0xE.0x3\"}'"
-      "-device '{\"driver\":\"pcie-root-port\",\"port\":20,\"chassis\":5,\"id\":\"pci.5\",\"bus\":\"pcie.0\",\"addr\":\"0xE.0x4\"}'"
-      "-device '{\"driver\":\"pcie-root-port\",\"port\":21,\"chassis\":6,\"id\":\"pci.6\",\"bus\":\"pcie.0\",\"addr\":\"0xE.0x5\"}'"
-      "-device '{\"driver\":\"vfio-pci\",\"host\":\"0000:0e:00.0\",\"id\":\"hostdev1\",\"bus\":\"pci.3\",\"addr\":\"0x0\"}'"
-      "-device '{\"driver\":\"vfio-pci\",\"host\":\"0000:0e:00.1\",\"id\":\"hostdev1\",\"bus\":\"pci.4\",\"addr\":\"0x0\"}'"
-      "-device '{\"driver\":\"vfio-pci\",\"host\":\"0000:0e:00.2\",\"id\":\"hostdev1\",\"bus\":\"pci.5\",\"addr\":\"0x0\"}'"
-      "-device '{\"driver\":\"vfio-pci\",\"host\":\"0000:0e:00.3\",\"id\":\"hostdev1\",\"bus\":\"pci.6\",\"addr\":\"0x0\"}'"
-
-      */
-
     # pci device: -device vfio-pci,host=<bus>:<slot>.<func>,multifunction=on
     virtualisation.qemu.options = [
       "-vga none"
